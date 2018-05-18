39d9b7ffbbc31aa4931342c361e3de1a08896a23
Orit Freud <oritfreud@Orits-MacBook-Air.local>
2018-05-18 11:24:16 -0400

    added name to model

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,24 +1,24 @@
-// var router = require('express').Router();
-// var user = require('../controller/userController.js');
+var router = require('express').Router();
+var user = require('../controller/userController.js');
 
-// //----------User Accounts----------
+//----------User Accounts----------
 
-// router.post('/login', function(req, res) {
-// 	user.login(req, function(data) {
-// 		res.json(data);
-// 	});
-// });
+router.post('/login', function(req, res) {
+	user.login(req, function(data) {
+		res.json(data);
+	});
+});
 
-// router.post('/logout', function(req, res) {
-// 	user.logout(req, function(data) {
-// 		res.json(data);
-// 	});
-// });
+router.post('/logout', function(req, res) {
+	user.logout(req, function(data) {
+		res.json(data);
+	});
+});
 
-// router.post('/signup', function(req, res) {
-// 	user.signup(req, function(data) {
-// 		res.json(data);
-// 	});
-// });
+router.post('/signup', function(req, res) {
+	user.signup(req, function(data) {
+		res.json(data);
+	});
+});
 
-// module.exports = router;
+module.exports = router;
