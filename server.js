5d0d51b16f608ee8b2cf9f2e97bfa3ebbce4fbb1
Orit Freud <oritfreud@Orits-MacBook-Air.local>
2018-05-17 20:50:30 -0400

    creatd server.js and fixed errors in models

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("view/build"));
+}
+
+// Define API routes here
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/zigrig");
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./view/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
+
+
